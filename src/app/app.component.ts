c10b0e83cbc56fd347bc59e0383defe0c839eba3
luluyaya <u83x06dog@gmail.com>
2019-11-14 21:34:30 +0800

    logo-final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,10 +24,16 @@ export class AppComponent implements OnInit {
     { id: 3, title: '世新大學人文社會學院《人文社會學報》第20期徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
     { id: 4, title: '南華大學哲學與生命教育學報《揭諦》徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
     { id: 5, title: '國立高雄師範大學《高雄師大學報-人文與藝術類、教育與社會科學類、自然科學與科技類》徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
-    { id: 6, title: '優魯好棒', content: 'dfdfdfdfsf', showDateTime: new Date() },
-    { id: 6, title: '優魯好棒', content: 'dfdfdfdfsf', showDateTime: new Date() },
-    { id: 6, title: '優魯好棒', content: 'dfdfdfdfsf', showDateTime: new Date() },
-    { id: 6, title: '優魯好棒', content: 'dfdfdfdfsf', showDateTime: new Date() },
+    { id: 6, title: '教務處通知--欲申請提前一學期畢業學生，請注意申請期限', content: 'dfdfdfdfsf', showDateTime: new Date() },
+    { id: 7, title: '東吳大學外國語文學院《東吳外語學報》第48期徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 8, title: '世新大學人文社會學院《人文社會學報》第20期徵稿，歡迎本校師生及相關研究同仁踴躍投稿', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 9, title: '南華大學哲學與生命教育學報《揭諦》徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 10, title: '南華大學哲學與生命教育學報《揭諦》徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 11, title: '教務處通知--欲申請提前一學期畢業學生，請注意申請期限', content: 'dfdfdfdfsf', showDateTime: new Date() },
+    { id: 12, title: '東吳大學外國語文學院《東吳外語學報》第48期徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 13, title: '世新大學人文社會學院《人文社會學報》第20期徵稿，歡迎本校師生及相關研究同仁踴躍投稿', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 14, title: '南華大學哲學與生命教育學報《揭諦》徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
+    { id: 15, title: '南華大學哲學與生命教育學報《揭諦》徵稿，歡迎本校師生及相關研究同仁踴躍投稿。', content: '優魯好棒優魯好棒優魯好棒優魯好棒優魯好棒', showDateTime: new Date() },
   ]
   dataSource: MatTableDataSource<any>;
 
